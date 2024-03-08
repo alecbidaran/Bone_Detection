e595cd1dd7d20c4ec74c5d9d804e7ecc53d5bca4
Ali Bidaran <30821855+alecbidaran@users.noreply.github.com>
2024-03-08 14:54:22 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,3 +15,8 @@ def config_yaml_File(yaml_path,train_path,val_path):
     f.write(config_file_template)
     print("Yaml file saved.")
   
+def get_image_Size(image_path):
+  image_list=glob.glob(f"{image_path}/*")
+  img=cv2.imread(image_list[0])
+  img_shape=img.shape
+  return img_shape
